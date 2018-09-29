d15cd5ab8d79c44ed20acb35f0a6401d0da8dd2f
liu_du@gowild.cn <liu_du@gowild.cn>
2018-09-29 16:48:16 +0800

    first

---
+++ app/src/main/java/cn/eternal/godeepintojava/Mystack.java
@@ -0,0 +1,10 @@
+package cn.eternal.godeepintojava;
+
+/**
+ * @author liudu
+ * @version 1.0a
+ * <p><strong>Features draft description.主要功能介绍</strong></p>
+ * @since 2018/9/29 16:47
+ */
+public class Mystack {
+}
